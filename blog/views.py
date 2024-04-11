cbc18fce60cedf76b79141456c404ef525ef0e21
Charan30092004 <charanvelumani@gmail.com>
2024-04-11 20:10:07 +0530

    Database and Migration

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import post
 
-post=[
-    {
-        'title':'Love',
-        'author':'Charan',
-        'content':'Secret of Love',
-        'date_posted':'30 Sep 2004'
-    }
-    ,    {
-        'title':'Friendship',
-        'author':'Aku',
-        'content':'Secret of Friendship',
-        'date_posted':'14 Nov 2004'
-    }
-]
 
 def home(request):
-    posts={
-        'posts':post,
-        'title':'Home'
-    }
+    posts = {"posts": post.objects.all(), "title": "Home"}
     return render(request,'blog/home.html',posts)
 
 def about(request):
