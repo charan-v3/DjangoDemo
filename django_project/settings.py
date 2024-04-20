80b3b4d1a5c9cb1d628e668db63b4bcfd584a8e0
Charan30092004 <charanvelumani@gmail.com>
2024-04-20 16:23:55 +0530

    first commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "sample",
+        "USER": "root",
+        "PASSWORD": "Charan@2023",
+        "HOST": "127.0.0.1",  # or the hostname where your MySQL server is running
+        "PORT": "3306",  # or the port on which your MySQL server is listening
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.register, name="user-register"),
+    path("", views.register, name="user_register"),
 ]
